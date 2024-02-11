e27bdeb6e807f4a6bc9e8550818cede1e0f14605
choiw26 <149434934+choiw26@users.noreply.github.com>
2024-02-11 15:03:28 -0500

    Added Feb11

---
+++ TenMinutePhysics/Random_Stuff/import.js
@@ -0,0 +1,15 @@
+export default class Ball {
+    constructor (position, velocity, radius) {
+        this.position.x = position.x;
+        this.position.y = position.y;
+
+        this.velocity.x = velocity.x;
+        this.velocity.y = velocity.y;
+        
+        this.radius = radius;
+    }
+}
+
+export function BallMovement (ball, force, dt) {
+    v
+}
